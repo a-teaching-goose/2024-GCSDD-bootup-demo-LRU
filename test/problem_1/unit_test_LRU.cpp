15f3102f402e2c54d1f97b00fbf3491382126fbc
Peng Du <rick.peng.du@gmail.com>
2024-07-31 13:46:18 -0700

    add unit test for lru

--- test/problem_1/unit_test_LRU.cpp
+++ test/problem_1/unit_test_LRU.cpp
@@ -5,45 +5,84 @@
 #include "gtest/gtest.h"
 #include "problem_1/LRU.hpp"
 
-TEST(problem_1, LRU) {
-    LRUCache<int> lru(5);
-
+TEST(problem_1, LRU_put_get) {
     int evicted_value;
+    LRUCache<int> lru(5);
+    std::vector<std::pair<std::string, int>> test_cases = {
+            {"a", 1},
+            {"b", 2},
+            {"c", 3},
+            {"d", 4},
+            {"e", 5},
+    };
+    for (auto &test_case: test_cases) {
+        bool evicted_item = lru.put(test_case.first, test_case.second, evicted_value);
+        ASSERT_FALSE(evicted_item);
+    }
 
-    lru.put("a", 1, evicted_value);
-    lru.put("b", 2, evicted_value);
-    lru.put("c", 3, evicted_value);
-    lru.put("d", 4, evicted_value);
-    lru.put("e", 5, evicted_value);
+    for (auto &test_case: test_cases) {
+        int actual;
+        bool found = lru.get(test_case.first, actual);
+        ASSERT_TRUE(found);
+        ASSERT_EQ(test_case.second, actual);
+    }
+}
 
-    // e->d->c->b->a
+TEST(problem_1, eviction_least_recently_used) {
+    int evicted_value;
+    LRUCache<int> lru(4);
+    std::vector<std::pair<std::string, int>> test_cases = {
+            {"a", 1},
+            {"b", 2},
+            {"c", 3},
+            {"d", 4},
+            {"e", 5},
+            {"f", 6},
+            {"g", 7},
+            {"h", 8},
+    };
 
-    lru.put("f", 6, evicted_value);
+    /*
+     * put items to lru to the limit (4)
+     */
+    int half = (int) test_cases.size() / 2;
+    for (int i = 0; i < half; ++i) {
+        auto test_case = test_cases[i];
+        lru.put(test_case.first, test_case.second, evicted_value);
+    }
 
-    // f->e->d->c->b
+    /*
+     * put 4 more items, and check for eviction
+     * before pushing one element:  vector [5, 6, 7, 8] => lru {4, 3, 2, 1}
+     * after pushing one element:   vector [6, 7, 8] => lru {5, 4, 3, 2} evicted 1
+     */
+    std::vector<std::pair<std::string, int>> items_in_lru(test_cases.begin(), test_cases.begin() + half);
+    std::reverse(items_in_lru.begin(), items_in_lru.end());
 
-    int val;
-    bool found = lru.get("a", val);
+    // items_in_lru here is {4, 3, 2, 1}
+    // Each time, the data at the end of items_in_lru will be evicted. 1 goes first, and then 2, etc.
 
-    found = lru.get("f", val);
+    for (int i = 0; i < half; ++i) {
+        auto test_case = test_cases[half + i];
+        bool evicted_item = lru.put(test_case.first, test_case.second, evicted_value);
 
-    found = lru.get("b", val);
+        // Verify:
+        // 1. Eviction happens
+        // 2. The right item was evicted
+        // 3. The evicted item does not exist in the lru
+        ASSERT_TRUE(evicted_item);
+        ASSERT_EQ(test_cases[i].second, evicted_value);
+        auto found = lru.get(items_in_lru.end()->first, evicted_value);
+        ASSERT_FALSE(found);
 
-    lru.remove("c");
-    // f->e->d->b
-
-    found = lru.get("c", val);
-
-    lru.put("x", 55, evicted_value);
-
-    // x->f->e->d->b
-
-    lru.put("y", 31, evicted_value);
-
-    // y->x->f->e->d
-    found = lru.get("x", val);
-
-    found = lru.get("y", val);
-
-    found = lru.get("b", val);
+        // Verify lru has all what it should have
+        items_in_lru.pop_back();
+        items_in_lru.insert(items_in_lru.begin(), test_cases[half + i]);
+        for (auto &item: items_in_lru) {
+            int actual;
+            found = lru.get(item.first, actual);
+            ASSERT_TRUE(found);
+            ASSERT_EQ(item.second, actual);
+        }
+    }
 }
